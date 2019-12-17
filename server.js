bc9bffbf048cc9794192a68e33396ec81811fc25
Sean Richter <sean.richter04@outlook.com>
2019-12-17 10:51:41 -0600

    initial commit - front end and back end working together

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ("express");
+
+const app = express();
+
+app.get("/api/customers", (req, res) => {
+    const customers = [
+        {id: 1, firstName: "Robin", lastName: "Trower"},
+        {id: 2, firstName: "Bill", lastName: "Withers"},
+        {id: 3, firstName: "Roy", lastName: "Buchanan"}
+    ];
+
+    res.json(customers);
+});
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
