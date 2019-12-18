b74ec1e5b1047854c3dc9a1c906b62d237e7e984
Sean Richter <sean.richter04@outlook.com>
2019-12-18 08:35:17 -0600

    added more npm packages - completed server.js and is working

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require ("express");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
+const PORT = 5000;
 const app = express();
 
-app.get("/api/customers", (req, res) => {
-    const customers = [
-        {id: 1, firstName: "Robin", lastName: "Trower"},
-        {id: 2, firstName: "Bill", lastName: "Withers"},
-        {id: 3, firstName: "Roy", lastName: "Buchanan"}
-    ];
+// Defining middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    res.json(customers);
+// Static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes
+app.use(routes);
+
+//http://localhost:5000/api/customers
+app.get("/api/customers", (req, res) => {
+  const customers = [
+    { id: 1, firstName: "Robin", lastName: "Trower" },
+    { id: 2, firstName: "Bill", lastName: "Withers" },
+    { id: 3, firstName: "Roy", lastName: "Buchanan" }
+  ];
+
+  res.json(customers);
 });
 
-const port = 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
