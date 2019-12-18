a398ebc5d476fb8c58061bbb5ecc797d40f766d5
Sean Richter <sean.richter04@outlook.com>
2019-12-18 09:49:10 -0600

    search function is working, saved function is not working

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -1,18 +1,17 @@
-
 import React from "react";
 import Header from "../components/Header";
 
 function NoMatch() {
   return (
     <div>
-          <Header>
-            <h1>404 Page Not Found</h1>
-            <h1>
-              <span role="img" aria-label="Face With Rolling Eyes Emoji">
-                🙄
-              </span>
-            </h1>
-          </Header>
+      <Header>
+        <h1>404 Page Not Found</h1>
+        <h1>
+          <span role="img" aria-label="Face With Rolling Eyes Emoji">
+            🙄
+          </span>
+        </h1>
+      </Header>
     </div>
   );
 }
